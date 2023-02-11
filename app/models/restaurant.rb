749b7e7a39080840450363dbf54b7ea32a5d3c9a
Emmanuel Carre <emma1601@live.com>
2023-02-11 14:16:19 +0400

    Restaurant_Model

---
+++ app/models/restaurant.rb
@@ -0,0 +1,9 @@
+class Restaurant < ApplicationRecord
+  belongs_to :user
+  has_many :reviews, dependent: :destroy
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :contact_number, presence: true
+  validates :lattitude, presence: true
+  validates :longitude, presence: true
+end
