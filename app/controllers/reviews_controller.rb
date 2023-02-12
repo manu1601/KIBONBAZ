b5a6076ca20d8db088f6b8546430f38863e4961b
vendana sahye taucoory <vensahye@gmail.com>
2023-02-12 21:56:50 +0400

    review_controller

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,35 @@
+class ReviewsController < ApplicationController
+  before_action :set_restaurant, only: %i[new create]
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+    @review.user = current_user
+    @review.save
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to restaurant_path(@review.restaurant), status: :see_other
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:comment, :rating)
+  end
+end
