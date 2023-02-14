f9934682a7135ef78721a5d737f4447e900ec48a
SHAHEEN GOKHOOL <bshine772@gmail.com>
2023-02-14 17:05:26 +0400

    populated db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,132 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "open-uri"
+puts 'populating database...'
+puts 'creating 3 users as owner'
+u1 = User.new(email: "sam@lewagon.com", password: "123456", username: "@sam", owner: true)
+u1.save!
+u2 = User.new(email: "Tom@lewagon.com", password: "789012", username: "@tom", owner: true)
+u2.save!
+u3 = User.new(email: "fifi@lewagon.com", password: "345678", username: "@fifi", owner: true)
+u3.save!
+puts " Finished creating 3 users as owner "
+
+puts "creating restaurants"
+file = URI.open("https://images.pexels.com/photos/262978/pexels-photo-262978.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2")
+r1 = Restaurant.new(name: "Elizabeth B&G", address: "Imam Bacos Sobdar St, Port Louis", contact_number: "52554898", user_id: 1)
+r1.photo.attach(io: file, filename: "r1.jpeg", content_type: "image/jpeg")
+r1.save!
+puts " Finished creating first restaurant"
+w1 = Working_hours.new(day: "Tuesday to Saturday", opening_hours: "12:00", closing_hours: "23:30", restaurant_id: 1)
+w1.save!
+puts " Finished creating first restaurant working hours w1"
+w2 = Working_hours.new(day: "Sunday", opening_hours: "16:00", closing_hours: "23:30", restaurant_id: 1)
+w2.save!
+puts " Finished creating first restaurant working hours w2"
+
+file = URI.open("https://images.pexels.com/photos/67468/pexels-photo-67468.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2")
+r2 = Restaurant.new(name: "Tariq snack", address: "Royal Road, Terminus , Triolet", contact_number: "57987532", user_id: 2)
+r2.photo.attach(io: file, filename: "r2.jpeg", content_type: "image/jpeg")
+r2.save!
+puts " Finished creating 2nd restaurant"
+w3 = Working_hours.new(day: "Tuesday to Saturday", opening_hours: "11:00", closing_hours: "20:00", restaurant_id: 2)
+w3.save!
+puts " Finished creating 2nd restaurant working hours w3"
+w4 = Working_hours.new(day: "Sunday", opening_hours: "12:00", closing_hours: "20:00", restaurant_id: 2)
+w4.save!
+puts " Finished creating 2nd restaurant working hours w4"
+
+file = URI.open("https://images.pexels.com/photos/958545/pexels-photo-958545.jpeg?auto=compress&cs=tinysrgb&w=1200")
+r3 = Restaurant.new(name: "Sameer snack", address: "Derningham, Triolet", contact_number: "57683085", user_id: 3)
+r3.photo.attach(io: file, filename: "r3.jpeg", content_type: "image/jpeg")
+r3.save!
+puts " Finished creating 3rd restaurant"
+w5 = Working_hours.new(day: "`Monday to Sunday", opening_hours: "9:00", closing_hours: "5:30", restaurant_id: 3)
+w5.save!
+puts " Finished creating 3rd restaurant working hours w5"
+
+file = URI.open("https://images.pexels.com/photos/1579739/pexels-photo-1579739.jpeg?auto=compress&cs=tinysrgb&w=1200")
+r4 = Restaurant.new(name: "Vona corona", address: "Royal Road, Rose-Hill", contact_number: "57222012", user_id: 1)
+r4.photo.attach(io: file, filename: "r4.jpeg", content_type: "image/jpeg")
+r4.save!
+puts " Finished creating 4th restaurant"
+w6 = Working_hours.new(day: "Monday to Sunday", opening_hours: "12:00", closing_hours: "23:00", restaurant_id: 4)
+w6.save!
+puts " Finished creating 4th restaurant working hours w6"
+puts " Finished creating restaurants"
+
+puts "creating tags"
+t1 = Tag.new(name_tag: "Briyani")
+t1.save!
+t2 = Tag.new(name_tag: "Kebab")
+t2.save!
+t3 = Tag.new(name_tag: "fried-noodles")
+t3.save!
+t4 = Tag.new(name_tag: "ice-cream")
+t4.save!
+t5 = Tag.new(name_tag: "mine frites")
+t5.save!
+t6 = Tag.new(name_tag: "dholl puri")
+t6.save!
+t7 = Tag.new(name_tag: "roti")
+t7.save!
+t8 = Tag.new(name_tag: "Burger")
+t8.save!
+t9 = Tag.new(name_tag: "BBQ")
+t9.save!
+puts " Finished creating 9 tags"
+
+puts 'creating 3 users as not -owner'
+u4 = User.new(email: "alex@lewagon.com", password: "645783", username: "@alex", owner: false)
+u4.save!
+u5 = User.new(email: "Jessy@lewagon.com", password: "5436723", username: "@Jessy", owner: false)
+u5.save!
+u6 = User.new(email: "claire@lewagon.com", password: "6543218", username: "@claire", owner: false)
+u6.save!
+puts " Finished creating 3 users as not owner "
+
+puts "creating favourites for users not owner "
+f1 = Favourite.new(user_id: 4, restaurant_id: 1)
+f1.save!
+f2 = Favourite.new(user_id: 5, restaurant_id: 2)
+f2.save!
+f3 = Favourite.new(user_id: 6, restaurant_id: 3)
+f3.save!
+f4 = Favourite.new(user_id: 4, restaurant_id: 3)
+f4.save!
+puts " finished creating favourites for users not owner "
+
+puts "creating favourites for users not owner "
+f1 = Favourite.new(user_id: 4, restaurant_id: 1)
+f1.save!
+f2 = Favourite.new(user_id: 5, restaurant_id: 2)
+f2.save!
+f3 = Favourite.new(user_id: 6, restaurant_id: 3)
+f3.save!
+f4 = Favourite.new(user_id: 4, restaurant_id: 3)
+f4.save!
+puts " finished creating favourites for users not owner "
+
+puts "creating suggestions"
+s1 = Suggestion.new(tag_id: 1, restaurant_id: 2)
+s1.save!
+s2 = Suggestion.new(tag_id: 1, restaurant_id: 3)
+s2.save!
+s3 = Suggestion.new(tag_id: 2, restaurant_id: 1)
+s3.save!
+s4 = Suggestion.new(tag_id: 2, restaurant_id: 2)
+s4.save!
+s5 = Suggestion.new(tag_id: 4, restaurant_id: 4)
+s5.save!
+puts " finished creating suggestions"
+
+puts "creating reviews by users not owner"
+re1 = Review.new(comments: "good food", rating: 3, restaurant_id: 1, user_id: 5)
+re1.save!
+re2 = Review.new(comments: "yummy!", rating: 4, restaurant_id: 2, user_id: 4)
+re2.save!
+re3 = Review.new(comments: "delicious!", rating: 5, restaurant_id: 4, user_id: 6)
+re3.save!
+
+puts "Finished populating database!"
