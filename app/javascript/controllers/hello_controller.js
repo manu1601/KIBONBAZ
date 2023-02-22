7cfe0495f69c236b45bb4e2196ed955682ca8ae8
SHAHEEN GOKHOOL <bshine772@gmail.com>
2023-02-22 15:08:11 +0400

    stimulus implemented to change icon color

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,7 +1,18 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = ["star"]
   connect() {
-    this.element.textContent = "Hello World!"
+    //this.element.textContent = "Hello World!"
+    console.log("hi from hello_controller.js ")
+    // console.log(this.element)
+    // console.log(this.starTarget)
+  }
+  favourite(event) {
+    // console.dir(this.starTarget)
+    console.log(this.starTarget.classList)
+    this.starTarget.classList.remove("unfavourite-icon")
+    this.starTarget.classList.add("favourite-icon")
+
   }
 }
