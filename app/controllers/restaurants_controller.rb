873606e255228cc9174b91108e9422e28cda6301
SHAHEEN GOKHOOL <bshine772@gmail.com>
2023-02-23 17:06:06 +0400

    added star icon to restaurants favourited by user

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,5 @@
 class RestaurantsController < ApplicationController
-  skip_before_action :authenticate_user!, only: :index
+  skip_before_action :authenticate_user!, only: [:index, :show]
   def index
     @restaurants = Restaurant.all
   end
