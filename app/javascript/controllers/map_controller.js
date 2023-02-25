6c48259730babecd94f19d1d496da70674becf54
Emmanuel Carre <emma1601@live.com>
2023-02-25 16:05:47 +0400

    branch_omega

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -16,8 +16,6 @@ export default class extends Controller {
     });
     this.#addMarkersToMap()
     this.#fitMapToMarkers()
-    //this.map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
-      //mapboxgl: mapboxgl }))
   }
 
   #fitMapToMarkers() {
@@ -40,32 +38,3 @@ export default class extends Controller {
     })
   }
 }
-
-// mapboxgl.accessToken = 'pk.eyJ1IjoibWFudTE2MDEiLCJhIjoiY2xlN2N2bmt5MDRhZTNxbnp2cGdoOHlrNCJ9.xO5W6wuqoE3YTEqTRkU6Fg';
-// const map = new mapboxgl.Map({
-// container: 'map',
-// // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
-// style: 'mapbox://styles/mapbox/streets-v12',
-// center: [-24, 42], // starting center in [lng, lat]
-// zoom: 1 // starting zoom
-// });
-
-// map.addControl(
-// new MapboxDirections({
-// accessToken: mapboxgl.accessToken
-// }),
-// 'top-left'
-// );
-
-
-// map.addControl(
-//     new mapboxgl.GeolocateControl({
-//       positionOptions: {
-//         enableHighAccuracy: true
-//   },
-//   // When active the map will receive updates to the device's location as it changes.
-//   trackUserLocation: true,
-//   // Draw an arrow next to the location dot to indicate which direction the device is heading.
-//   showUserHeading: true
-//   })
-//   );
